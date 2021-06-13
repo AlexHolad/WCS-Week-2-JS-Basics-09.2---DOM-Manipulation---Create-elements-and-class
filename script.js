18f38a45bcf86b5b3537e0e526b20074832478bf
AlexHolad <alexej.holad@gmail.com>
2021-06-13 18:01:12 +0200

    changed styles h1 and div

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+const title = document.querySelector(".title")
+const box = document.querySelector(".box")
+
+box.style.backgroundColor = "yellow"
+box.style.height = "300px"
+box.style.width = '400px'
+
+title.style.fontSize = '60px'
+title.style.textAlign = 'center'
+title.style.color = 'lightblue'
